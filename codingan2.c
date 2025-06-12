fe22c065bee955a5010d7788b4be780a631ef43c
madiduts <mahdisutejo7@gmail.com>
2025-06-12 15:10:42 +0700

    Whoosh

---
+++ codingan2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h> // Untuk generate_id yang lebih acak atau timestamp
+
+
+#define STATUS_TERSEDIA 0
+#define STATUS_TERISI 1
+
+typedef struct kereta {
+    int id, harga; 
+    char kodeKereta[50], rute[50], jadwalKeberangkatan[50];
+} kereta;
+
+typedef struct kursi {
+    int nomorKursi, status; // 0 = Tersedia, 1 = Terisi
+} kursi;
+
+typedef struct penumpang {
+    int idPenumpang; // Bisa di-generate otomatis
+    char namaLengkap[100], nik[20], noTelepon[20], email[50];
+} penumpang;
+
+typedef struct tiket{
+    int totalBayar;
+    char kodeBooking[15], namaPenumpang[20], tanggalKeberangkatan[20], waktuKeberangkatan[10];
+    penumpang dataPenumpang; // Data penumpang untuk tiket ini
+} tiket;
+
+typedef struct stasiun {
+    int idStasiun;
+    char kodeKereta[50], kotaAsal[50], kotaTujuan[50];
+} stasiun;
+
+typedef struct jadwalKereta {
+    int hargaFirstClass, hargaBisnis, hargaEkonomi;
+    char kodeKereta[50], waktuBerangkat[10], waktuTiba[10];
+    stasiun kota; 
+} jadwalKereta;
+
+typedef struct node {
+    tiket dataTiket;
+    struct node *next;
+} node;
+
